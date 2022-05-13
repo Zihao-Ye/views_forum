b352e9b86e6455191c611a3c1c38187d0056f983
Zihao-Ye <70185800+Zihao-Ye@users.noreply.github.com>
2022-05-13 18:49:02 +0800

    实现系统消息的获取以及已读消息、一键已读

--- ViewsForum/src/main/java/com/viewsforum/viewsforum/Controller/MessageController.java
+++ ViewsForum/src/main/java/com/viewsforum/viewsforum/Controller/MessageController.java
@@ -1,27 +1,83 @@
 package com.viewsforum.viewsforum.Controller;
 
+import com.viewsforum.viewsforum.Entity.SystemMessage;
+import com.viewsforum.viewsforum.Service.SystemMessageService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @Api(tags = "消息相关接口")
 @RestController("/message")
 public class MessageController {
-    //todo 获取系统消息列表
+    @Autowired
+    private SystemMessageService systemMessageService;
 
-//    @GetMapping("/applyMessage")
-//    @ApiOperation("申请类消息")
-//    public Map<String,Object> applyMessage(@RequestParam Integer userID){
-//
-//    }
+    @GetMapping("differentMessage")
+    @ApiOperation("不同类别的消息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userID",value = "用户ID",required = true,dataType = "int"),
+            @ApiImplicitParam(name = "messageType",value = "消息类型 1：申请信息（创建者接收申请信息、申请者接收到拒绝/同意/被解除管理员信息）2：回复信息（帖子创建者接收到发言信息、发言/回复接收到回复信息） 3：关注信息（被关注信息） 4：警告/删除信息（主题被删除、帖子被删除、发言被删除、回复被删除）",required = true,dataType = "int")
+    })
+    public Map<String,Object> differentMessage(@RequestParam Integer userID,@RequestParam Integer messageType){
+        Map<String,Object> map=new HashMap<>();
+        try {
+            List<SystemMessage> systemMessageList=systemMessageService.getSystemMessageByUserIDAndMessageType(userID,messageType);
+            map.put("success",true);
+            map.put("systemMessageList",systemMessageList);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            map.put("success",false);
+            map.put("msg","INTERNAL_ERROR");
+        }
+        return map;
+    }
 
-    //todo 已读消息
+    @GetMapping("readOneMessage")
+    @ApiOperation("不同类别的消息")
+    @ApiImplicitParam(name = "systemMessageID",value = "系统消息ID",required = true,dataType = "int")
+    public Map<String,Object> readOneMessage(@RequestParam Integer systemMessageID){
+        Map<String,Object> map=new HashMap<>();
+        try {
+            systemMessageService.readOneSysTemMessage(systemMessageID);
+            map.put("success",true);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            map.put("success",false);
+            map.put("msg","INTERNAL_ERROR");
+        }
+        return map;
+    }
+
+    @PostMapping("readAllMessage")
+    @ApiOperation("一键已读消息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userID",value = "用户ID",required = true,dataType = "int"),
+            @ApiImplicitParam(name = "messageType",value = "消息类型 1：申请信息（创建者接收申请信息、申请者接收到拒绝/同意/被解除管理员信息）2：回复信息（帖子创建者接收到发言信息、发言/回复接收到回复信息） 3：关注信息（被关注信息） 4：警告/删除信息（主题被删除、帖子被删除、发言被删除、回复被删除）",required = true,dataType = "int")
+    })
+    public Map<String,Object> readAllMessage(@RequestParam Integer userID,@RequestParam Integer messageType){
+        Map<String,Object> map=new HashMap<>();
+        try {
+            systemMessageService.readAllSystemMessage(userID,messageType);
+            map.put("success",true);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            map.put("success",false);
+            map.put("msg","INTERNAL_ERROR");
+        }
+        return map;
+    }
 
     //todo websocket实现实时通讯
 }
