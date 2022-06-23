1f34b506257d7069def46d0055812eff673593d0
Ashen <ashen706@163.com>
2022-06-23 16:38:38 +0800

    私信部分测试完毕

--- ViewsForum/src/main/java/com/viewsforum/viewsforum/Entity/Chat.java
+++ ViewsForum/src/main/java/com/viewsforum/viewsforum/Entity/Chat.java
@@ -17,7 +17,7 @@ import java.sql.Timestamp;
 @ApiModel(description = "私聊")
 public class Chat {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @ApiModelProperty(value = "聊天ID")
     private Integer chatID;
 
